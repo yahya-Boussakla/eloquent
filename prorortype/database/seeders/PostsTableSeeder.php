1648704e0575a1945c4fc18d12dd5f0dd18b12d0
yahya <yahyaboussakl@gmail.com>
2024-11-01 09:39:16 +0100

    prototype

--- prorortype/database/seeders/PostsTableSeeder.php
+++ prorortype/database/seeders/PostsTableSeeder.php
@@ -2,18 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class CategoriesTableSeeder extends Seeder
+class PostsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Category::factory(10)->create();
-        // Category::factory()->count(5)->active()->create();
+        //
     }
 }
